b44735771f503466b03a00be9d27bad50d39453c
wumingzi <1078249029@qq.com>
2024-01-16 11:27:19 +0800

    恢复了误删的文件并且添加了main.c

--- freertos/Source/main.c
+++ freertos/Source/main.c
@@ -1,7 +1,7 @@
 
 #include "FreeRTOS.h"
 #include "task.h"
-
+#include "semaphore.h"
 
 portCHAR flag1;
 portCHAR flag2;
@@ -35,10 +35,20 @@ void delay (uint32_t count);
 void Task1_Entry( void *p_arg );
 void Task2_Entry( void *p_arg );
 void Task3_Entry( void *p_arg );
+void Package1(void);
+void Package2(void);
+
+
+SemaphoreHandle_t SemaphoreHandle1;
+Semaphore_t Semaphore1;
 
 
 int main(void)
 {	
+	
+	
+	SemaphoreHandle1 = SemaphoreCreat(&Semaphore1, 2, 1);
+	SemaphoreGive(&SemaphoreHandle1);
 
     Task1_Handle = xTaskCreateStatic( (TaskFunction_t)Task1_Entry,  
 					                  (char *)"Task1",              
@@ -60,7 +70,7 @@ int main(void)
 					                  (char *)"Task3",              
 					                  (uint32_t)TASK3_STACK_SIZE ,  
 					                  (void *) NULL,                
-                                      (UBaseType_t) 3,                                                    
+                                      (UBaseType_t) 1,                                                    
 					                  (StackType_t *)Task3Stack,    
 					                  (TCB_t *)&Task3TCB );                          
                                       
@@ -79,16 +89,14 @@ void delay (uint32_t count)
 	for(; count!=0; count--);
 }
 
+
 void Task1_Entry( void *p_arg )
 {
 	for( ;; )
 	{
-		flag1 = 1;
-        //vTaskDelay( 1 );
-        delay (100);		
-		flag1 = 0;
-        delay (100);
-        //vTaskDelay( 1 );        
+		SemaphoreTake(&SemaphoreHandle1, portMAX_DELAY);
+		Package1(); 
+		SemaphoreGive(&SemaphoreHandle1);
 	}
 }
 
@@ -96,12 +104,9 @@ void Task2_Entry( void *p_arg )
 {
 	for( ;; )
 	{
-		flag2 = 1;
-        //vTaskDelay( 1 );
-        delay (100);		
-		flag2 = 0;
-        delay (100);
-        //vTaskDelay( 1 );        
+		SemaphoreTake(&SemaphoreHandle1, portMAX_DELAY);
+		Package2();
+		SemaphoreGive(&SemaphoreHandle1);
 	}
 }
 
@@ -116,6 +121,30 @@ void Task3_Entry( void *p_arg )
 		flag3 = 0;
         vTaskDelay( 1 );
         //delay (100);
+	}
+}
+
+void Package1(void)
+{
+	int i = 0;
+	for(i = 0; i < 100000; i++)
+	{
+		flag1 = 0;
+		delay(10);
+		flag1 = 1;
+		delay(10);
+	}
+}
+
+void Package2(void)
+{
+	int i = 0;
+	for(i = 0; i < 10; i++)
+	{
+		flag2 = 0;
+		delay(1000000);
+		flag2 = 1;
+		delay(1000000);
 	}
 }
 
